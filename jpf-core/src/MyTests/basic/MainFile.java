0d57eacc4850f7e4c8d401cc68745e1ce8b80208
Jonas Traweek <jonastraweek@yahoo.com>
2022-11-02 16:24:17 -0500

    New JPF folder

---
+++ jpf-core/src/MyTests/basic/MainFile.java
@@ -0,0 +1,49 @@
+
+
+public class MainFile implements Runnable {
+	private static int sum = 0;
+
+	static Object lock1 = new Object();
+	static Object lock2 = new Object();
+	final int repetitions=100;
+
+	
+	public void foo(Object A, Object B) {
+		synchronized (A) {
+			synchronized (B) {
+				for (int i = 0; i < repetitions; i++) {
+					sum++;
+				
+				}
+			}
+		}
+	}
+
+	public void bar(Object A, Object B) {
+		synchronized (B) {
+			synchronized (A) {
+				for (int i = 0; i < repetitions; i++) {
+					sum--;
+			
+				}
+			}
+		}
+	}
+
+	public int getSum(Object A) {
+		synchronized (A) {
+			return sum;
+		}
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+		for (int x = 0; x < 2; x++) {
+			foo(lock1, lock2);
+			bar(lock1, lock2);
+		}
+		//System.out.println(getSum(lock1));
+	}
+}
---
+++ jpf-core/src/MyTests/basic/StartCode.java
@@ -0,0 +1,25 @@
+
+
+public class StartCode {
+	public static void main(String[] args) {
+		int numThreads=5;
+		Thread list[] = new Thread[numThreads];
+		for (int i = 0; i < numThreads; i++) {
+			Thread t = new Thread(new MainFile());
+			list[i] = t;
+			t.start();
+		}
+
+		for (Thread thread : list) {
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.println("Finished Regular");
+		
+	}
+}
